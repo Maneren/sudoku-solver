37120c04601b389caa015eaf5e5790164ecb834d
Maneren <49210777+Maneren@users.noreply.github.com>
2021-06-02 00:26:52 +0200

    done

---
+++ src/board.rs
@@ -0,0 +1,102 @@
+pub struct Board {
+  data: [[Option<u32>; 9]; 9],
+}
+
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+pub struct BoardError {
+  msg: String,
+}
+
+impl fmt::Display for BoardError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{}", self.msg)
+  }
+}
+
+impl Error for BoardError {
+  fn source(&self) -> Option<&(dyn Error + 'static)> {
+    None
+  }
+}
+
+pub type Tile = Option<u32>;
+pub type TilePointer = (usize, usize);
+pub type Row = [Tile; Board::SIZE];
+// pub type Column = [Tile; Board::SIZE];
+
+impl Board {
+  pub const SIZE: usize = 9;
+
+  pub fn new(input_data: Vec<Vec<Option<u32>>>) -> Result<Board, BoardError> {
+    if input_data.len() != Self::SIZE {
+      return Err(BoardError {
+        msg: "Invalid row count".to_string(),
+      });
+    }
+
+    let mut data: [Row; Self::SIZE] = [[None; Self::SIZE]; Self::SIZE];
+
+    for (i, row) in input_data.iter().enumerate() {
+      if row.len() != Self::SIZE {
+        return Err(BoardError {
+          msg: "Invalid columns count".to_string(),
+        });
+      }
+
+      for (j, tile) in row.iter().enumerate() {
+        data[i][j] = tile.to_owned();
+      }
+    }
+
+    Ok(Board { data })
+  }
+
+  // pub fn get_row(&self, index: usize) -> Row {
+  //   self.data[index]
+  // }
+
+  // pub fn get_column(&self, index: usize) -> Column {
+  //   let mut column: Column = [None; Board::SIZE];
+  //   for i in 0..Board::SIZE {
+  //     column[i] = self.data[i][index];
+  //   }
+  //   column
+  // }
+
+  pub fn get_tile(&self, ptr: TilePointer) -> Tile {
+    let (x, y) = ptr;
+    self.data[y][x]
+  }
+
+  pub fn set_tile(&mut self, ptr: TilePointer, value: Option<u32>) {
+    let (x, y) = ptr;
+    self.data[y][x] = value;
+  }
+
+  pub fn clone(&self) -> Self {
+    Board { data: self.data }
+  }
+}
+
+impl fmt::Display for Board {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    let string: String = self
+      .data
+      .iter()
+      .map(|row| {
+        row
+          .iter()
+          .map(|field| match field {
+            Some(num) => num.to_string(),
+            None => "-".to_string(),
+          })
+          .collect::<String>()
+          + "\n"
+      })
+      .collect::<String>();
+    write!(f, "{}", string)
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,204 @@
+// get argv
+use std::env;
+// files
+use std::fs::File;
+use std::io::prelude::*;
+
+use std::time::Duration;
+
+mod board;
+
+use board::Board;
+type Error = Box<dyn std::error::Error>;
 fn main() {
-    println!("Hello, world!");
+  println!("Solving!\n");
+
+  match &env::args().collect::<Vec<String>>()[..] {
+    [_, input] => match run(input, false) {
+      Ok(_) => println!("Done!"),
+      Err(msg) => panic!("{}", msg),
+    },
+    [_, input, _] => match run(input, true) {
+      Ok(_) => println!("Done!"),
+      Err(msg) => panic!("{}", msg),
+    },
+    _ => println!("Invalid arguments"),
+  }
+}
+
+fn run(path_to_input: &str, verbose: bool) -> Result<(), Error> {
+  let input_string = load_input(&path_to_input)?;
+  let board = parse_board(&input_string)?;
+
+  println!("{}", board);
+
+  let start = std::time::Instant::now();
+  let solved = solve(&board, verbose)?;
+
+  if !verbose {
+    println!("{}", solved);
+    println!("Time taken: {} ms", start.elapsed().as_millis());
+  };
+
+  Ok(())
+}
+
+fn load_input(path: &str) -> Result<String, Error> {
+  let mut file = File::open(path)?;
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)?;
+  Ok(contents)
+}
+
+fn parse_board(input_string: &str) -> Result<Board, Error> {
+  let rows = input_string
+    .split('\n')
+    .map(|row| row.chars().collect::<Vec<char>>())
+    .collect::<Vec<Vec<char>>>();
+
+  let board = board::Board::new(
+    rows
+      .iter()
+      .map(|row| row.iter().map(|ch| ch.to_digit(10)).collect())
+      .collect(),
+  )?;
+
+  Ok(board)
+}
+
+fn solve(board: &Board, verbose: bool) -> Result<Board, Error> {
+  let mut board = board.clone();
+
+  let mut empty_fields: Vec<board::TilePointer> = vec![];
+
+  for y in 0..Board::SIZE {
+    for x in 0..Board::SIZE {
+      let tile = board.get_tile((x, y));
+      if tile == None {
+        empty_fields.push((x, y));
+      }
+    }
+  }
+
+  if empty_fields.is_empty() {
+    return Ok(board);
+  }
+
+  if solve_loop(&mut board, &empty_fields, 0, &mut 0, verbose) {
+    if verbose {
+      reprint(&board);
+    }
+    Ok(board)
+  } else {
+    Err("Unsolvable".into())
+  }
+}
+
+fn reprint(board: &Board) {
+  // for _ in 0..Board::SIZE {
+  //   println!("\x1b[1K\r \x1b[A")
+  // }
+  print!("{esc}c", esc = 27 as char);
+  println!("{}", board);
+}
+
+fn solve_loop(
+  board: &mut Board,
+  empty_fields: &[board::TilePointer],
+  current_index: usize,
+  iteration: &mut u32,
+  verbose: bool,
+) -> bool {
+  // println!("\n{}\n{}", current_index, board);
+  if verbose {
+    *iteration += 1;
+    if *iteration % 10 == 0 {
+      std::thread::sleep(Duration::from_millis(50));
+      reprint(&board);
+    }
+  }
+  if current_index == empty_fields.len() {
+    return true;
+  }
+
+  let current = empty_fields[current_index];
+
+  let possible_values: [u32; 9] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for value in possible_values.iter() {
+    board.set_tile(current, Some(value.to_owned()));
+    // println!("{:?} {:?} {}", current, value, is_valid(&board, &current));
+    if is_valid(&board, &current) {
+      if solve_loop(board, empty_fields, current_index + 1, iteration, verbose) {
+        return true;
+      } else {
+        board.set_tile(current, None);
+      }
+    }
+  }
+  board.set_tile(current, None);
+
+  false
+}
+
+fn is_valid(board: &Board, last_play: &board::TilePointer) -> bool {
+  let mut seen: bool;
+  let (x, y) = last_play;
+  let last_play_value = board.get_tile(*last_play);
+  let mut current: board::TilePointer;
+
+  // row
+  current = (0, *y);
+  seen = false;
+  for _ in 0..Board::SIZE {
+    // println!("{:?}", current);
+    // println!("{:?} {:?}", board.get_tile(current), last_play_value);
+    if board.get_tile(current) == last_play_value {
+      if seen {
+        return false;
+      } else {
+        seen = true;
+      }
+    }
+    current.0 += 1;
+  }
+
+  // collumn
+  current = (*x, 0);
+  seen = false;
+  for _ in 0..Board::SIZE {
+    // println!("{:?}", current);
+    // println!("{:?} {:?}", board.get_tile(current), last_play_value);
+    if board.get_tile(current) == last_play_value {
+      if seen {
+        return false;
+      } else {
+        seen = true;
+      }
+    }
+    current.1 += 1;
+  }
+
+  // box
+  let box_x = x - x % 3;
+  let box_y = y - y % 3;
+  current = (box_x, box_y);
+  seen = false;
+
+  for _ in 0..3 {
+    for _ in 0..3 {
+      // println!("{:?}", current);
+      if board.get_tile(current) == last_play_value {
+        if seen {
+          return false;
+        } else {
+          seen = true;
+        }
+      }
+      current.0 += 1;
+    }
+    current.0 = box_x;
+    current.1 += 1;
+  }
+
+  true
 }
